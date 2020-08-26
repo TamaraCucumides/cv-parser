d95d082930b053b98e8fdb5aed55413284999338
Erwin <erwin.paillacan@ug.uchile.cl>
2020-08-26 12:47:54 -0400

    Se agregan el extraer nombres

--- parser/parser.py
+++ parser/parser.py
@@ -1,21 +1,27 @@
 import utils
+import os
+import en_core_web_sm
 
+import pprint
 
 class CvParser:
     def __init__(self, cv):
+        nlp = en_core_web_sm.load()
         self.parsed = {"Nombre": None,
                        "Correo": None,
                        "Celular": None,
                        "Skills": None,
                        "Experiencia Previa": None,
                        "Educacion": None}
-        self.raw_text = cv
-
+        self.cv = cv
+        self.raw_text = utils.extract_text(self.cv)
+        self.text = ' '.join(self.raw_text.split())
+        self.nlp = nlp(self.text)
         self.parse()
         self._sections = dict()
 
     def parse(self):
-        nombre = None
+        nombre = utils.retrieve_name(self.raw_text)
         correo = utils.retrieve_email(self.raw_text)
         celular = utils.retrieve_phone_number(self.raw_text)
         skills = utils.retrieve_skills(self.raw_text)
@@ -35,9 +41,31 @@ class CvParser:
     def _sectionize(self):
         return utils.parse_cv_sections(self.raw_text)
 
+#resume = '/home/erwin/cv-parser/example_1.pdf'
+#parser = CvParser(resume)
+#parser.parse()
+
+def resume_result_wrapper(resume):
+        parser = CvParser(resume)
+        return parser.get_parsed_resume()
 
 
 
+if __name__ == '__main__':
+    resumes = []
+    data = []
+    for root, directories, filenames in os.walk('resumes'):
+        for filename in filenames:
+            file = os.path.join(root, filename)
+            resumes.append(file)
+    #print(resumes)
+
+    results = [resume_result_wrapper(x) for x in resumes]
+    print(results)
+
+    #results = [p.get_parse for p in results]
+
+    #pprint.pprint(results)
 
 
 
--- parser/utils.py
+++ parser/utils.py
@@ -1,21 +1,42 @@
 import re
 from cts import *
+import fitz
+from nltk.tokenize import sent_tokenize, word_tokenize
+import en_core_web_sm
+from spacy.matcher import Matcher
 
+def extract_text(path):
+    with fitz.open(path) as doc:
+        text = ""
+        for page in doc:
+            text += page.getText()
+        return text
 
 def retrieve_email(text):
     return re.findall('\S+@\S+', text)
 
 
 def retrieve_phone_number(text):
-    pass
+    regex = re.compile("\+?\d[\( -]?\d{3}[\) -]?\d{3}[ -]?\d{2}[ -]?\d{2}")
+    numbers = re.findall(regex, text)
+    #print(numbers)
+    return numbers
 
 
 def retrieve_skills(text):
-    return [skill for skill in skills if (' '+skill+' ') in text.lower()]
+    return [skill for skill in skills if (' '+skill.lower()+' ') in text.lower()]
 
 
 def retrieve_education_institution(text):
-    pass
+    #tokens = word_tokenize(text)
+    #print(tokens)
+    educacion_list=[]
+    for item in educacion:
+      if item.lower() in text.lower():
+          #print ('found one of em')
+          educacion_list.append(item)
+    return educacion_list
+
 
 
 def retrieve_higher_degree(text):
@@ -33,9 +54,28 @@ def retrieve_past_experience(text):
 def retrieve_last_experience_year(text):
     pass
 
-
+NAME_PATTERN      = [{'POS': 'PROPN'}, {'POS': 'PROPN'}, {'POS': 'PROPN'}]
 def retrieve_name(text):
-    pass
+
+    '''
+    Helper function to extract name from spacy nlp text
+    :param nlp_text: object of `spacy.tokens.doc.Doc`
+    :param matcher: object of `spacy.matcher.Matcher`
+    :return: string of full name
+    '''
+    nlp = en_core_web_sm.load()
+    matcher = Matcher(nlp.vocab)
+    pattern = [NAME_PATTERN]
+    nlp_text = nlp(text)
+    matcher.add('NAME', None, *pattern)
+    
+    matches = matcher(nlp_text)
+    #print(matches)
+    
+    for match_id, start, end in matches:
+        span = nlp_text[start:end]
+        #print(span.text)
+        return span.text
 
 
 def parse_cv_sections(text):
