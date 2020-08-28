f8f2aa578f5dc0172a3c0c88bdc32a48a081773b
Erwin <erwin.paillacan@ug.uchile.cl>
2020-08-28 14:48:54 -0400

    Se agregan universiades, siglas, etc.

--- parser/cts.py
+++ parser/cts.py
@@ -15,10 +15,10 @@ sections = {"resumen": ["Resumen"],
             "educacion": ["Educación", "Formación", "Antecedentes de formación"]}
 
 educacion = ["Universidad de Chile", "UChile","Pontificia Universidad Católica de Chile",
-                "Universidad de los Lagos"]
+                "Universidad de los Lagos", "Universidad de Santiago de Chile", "Universidad Tecnológica Metropolitana", "Universidad Diego Portales"
+                , 'Universidad del Desarrollo', 'Universidad Austral de Chile', 'Inacap']
 
-
-educacionSiglas = ['UC', 'UTEM', 'UAI', 'PUC', 'UCH', 'UDD', 'UAI', 'ULA']
+educacionSiglas = ['UC', 'UTEM', 'UAI', 'PUC', 'UCH', 'UDD', 'UAI', 'ULA', 'USACH', 'UACH', 'DUOC']
 
 idiomas = ['inglés', 'alemán', 'portugués', 'francés', 'chino', 'mandarin', 'español']
 idiomas_nivel = [' a1', ' a2', ' b1', ' b2', ' c1', ' c2', ' básico', ' intermedio', ' avanzado', ' nativo', ' competencia básica' 
@@ -26,7 +26,7 @@ idiomas_nivel = [' a1', ' a2', ' b1', ' b2', ' c1', ' c2', ' básico', ' interme
 
 skills = list(pd.read_csv(os.path.join(sys.path[0], "skills.csv")))
 
-palabras_claves = ['proactividad', 'proactivo', 'liderar', 'liderazgo', 'liderado', 'puntual'
-         , 'puntualidad', 'paciencia', 'resiliencia', 'resilisente', 'tolerancia', 'comprometido', 'compromiso', 'empatia', 'responsabilidad',
+palabras_claves = ['proactividad', 'liderar', 'liderazgo',  'puntual'
+         ,  'paciencia', 'resiliencia', 'resilisente', 'tolerancia', 'comprometido', 'compromiso', 'empatia', 'responsabilidad',
          'negociación', 'dialogo', 'escuchar', 'comunicacion','analizar', 'autodidacta', 'autoaprendizaje', 'adaptación'
-         'adaptar', 'creativo' ,'creatividad', 'ética', 'disciplina', 'confianza' , 'motivación', 'motivado', 'colaboración']
+         , 'creativo' , 'ética', 'disciplina', 'confianza' , 'motivación', 'colaboración']
